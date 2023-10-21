73e7ad95582af1fc7acd9846a1583bc3fa3d7b3b
inhoudblotter <inhoudblotter@gmail.com>
2023-10-22 04:40:13 +0500

    fix: click on a button in firefox

--- src/ui/SwitchButton/SwitchButton.jsx
+++ src/ui/SwitchButton/SwitchButton.jsx
@@ -4,9 +4,9 @@ import './SwitchButton.css';
 export function SwitchButton({onChange, blockName, className, icon}) {
   className = className || 'switch-btn';
   return (
-    <div className={`${blockName}__${className} switch-btn`}>
+    <label className={`${blockName}__${className} switch-btn`}>
       <input className={`switch-btn__checkbox`} type="checkbox" onChange={onChange} />
       {<Icon name={icon} blockName={'switch-btn'} />}
-    </div>
+    </label>
   );
 }
